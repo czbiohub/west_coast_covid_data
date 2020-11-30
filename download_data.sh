5db1a447d087b65c7e82151f44bfdf7d7ecb356d
Lucy Li <lucy.li@li-mbp.local>
2020-11-30 11:52:05 -0800

    data

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://covidtracking.com/data/download/california-history.csv
+wget https://covidtracking.com/data/download/oregon-history.csv
+wget https://covidtracking.com/data/download/washington-history.csv
