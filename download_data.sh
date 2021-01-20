7abab0f10244988179fde8bd4d539e403c05e6fd
Lucy Li <lucy.li@li-mbp.attlocal.net>
2021-01-20 15:19:43 -0800

    add county level data for the 3 west coast states

--- download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://covidtracking.com/data/download/california-history.csv
-wget https://covidtracking.com/data/download/oregon-history.csv
-wget https://covidtracking.com/data/download/washington-history.csv
